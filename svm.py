1f49208abb6c7b012fa9b4ff6ae421b1538864ca
WangYan <551619855@qq.com>
2015-03-12 13:11:13 +0800

    test on windows

--- svm.py
+++ svm.py
@@ -24,8 +24,8 @@ ans = numpy.zeros(len(pose_label))
 for i in range(0,len(pose_label)-1):
     ans[i] = clf.predict(wifi[i, :])
     err += (ans[i] - pose_label[i]) * (ans[i] - pose_label[i])
-    if abs(ans[i] - pose_label[i])>2:
-        err_times +=1
+    if abs(ans[i] - pose_label[i]) > 2:
+        err_times  = err_times + 1
         print 'ans:',ans[i],'pose_label: ',pose_label[i]#显示错误分类的结果，和真实类别
     #print err,'  ',err_times,' i:',i, 'i-err_times:' , i-err_times
 print 'err:',err,'err_times:',err_times

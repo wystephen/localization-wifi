236e3f2da20ce5efa51c065cf1b7c0e313734ed8
wangyan <551619855@qq.com>
2015-03-14 21:51:42 +0800

    封装完毕，精简了一下，可以测试新采的数据。

--- data_transfor.py
+++ data_transfor.py
@@ -17,7 +17,6 @@ def pose_to_label(pose, distance):
         if (pose[i,0]-pose[last_i,0])*(pose[i,0]-pose[last_i,0]) + \
                         (pose[i, 1]-pose[last_i, 1])* (pose[i, 1]-pose[last_i, 1])< distance * distance:
             label[i] = label[last_i]
-
         else:
             label[i] = label[last_i] + 1
             label_dict[label[i]] = [pose[i, 0], pose[i, 1]]
@@ -57,9 +56,7 @@ def get_mac_list(wifi_file,blue_file='null'):
     mac_list = fp.readlines()
     fp.close()
     return  mac_list
-
-
-def syn_data(pose_file ,wifi_file, pose_out, wifi_out)
+def syn_data(pose_file ,wifi_file, pose_out, wifi_out):
     fp = open(wifi_file,'rb')
     wifi_list = fp.readlines()
     fp.close()
@@ -70,22 +67,26 @@ def syn_data(pose_file ,wifi_file, pose_out, wifi_out)
     wifiout = open(wifi_out,'w')
     poseout = open(pose_out,'w')
     for wifi in wifi_list:
-        print wifi_list.index(wifi), ' summ', len(wifi_list)
+       # print wifi_list.index(wifi), ' in sum of ', len(wifi_list)
 
         wifi = wifi.split(' ')
+        if len(wifi) < 10:
+            print wifi
+            continue
         for laser in laser_list:
             laser = laser.split(' ')
-            if wifi[0] < laser[0]:
-                for i in range(1, len(wifi)-1):
+            if ((wifi[0] < laser[0]) and (wifi[0] > 14444.0))  :
+                for i in range(1, len(wifi)):
                     wifiout.write((wifi[i] + ' '))
-                poseout.write(laser[1] +' ' + laser[2] + '\n')
                 wifiout.write('\n')
+                poseout.write(laser[1] +' ' + laser[2] + '\n')
+
                 break
     wifiout.close()
     poseout.close()
     return
 #输入wifi文件，和mac——list文件，并且处理
-def file_trance(file, year, month, day, hours, minutes):
+def file_trance(file, year, month, day, hours):
     fp = open(file, 'rb')
     wifi = fp.readlines()
     fp.close()
@@ -93,7 +94,7 @@ def file_trance(file, year, month, day, hours, minutes):
     mac_list = fp.readlines()
     fp.close()
     fout = open('out_wifi.txt', 'w')
-    all_instance = numpy.zeros(mac_list.count())
+    all_instance = numpy.zeros(len(mac_list)+1)
     first = True
     print mac_list
     for line in wifi:
@@ -101,7 +102,7 @@ def file_trance(file, year, month, day, hours, minutes):
             continue
         if line[0] == '#':
             if not first:
-                for i in range(mac_list.count()):
+                for i in range(len(mac_list)+1):
                     fout.write((str(int(all_instance[i])) + ' '))
             fout .write('\n')
             line = line.split(' ')
@@ -122,3 +123,28 @@ def file_trance(file, year, month, day, hours, minutes):
 
     return
 #file_trance('sourcedata/wifi.txt',year='2015', month='01', day='28', hours='18', minutes='11')
+if __name__ == '__main__':
+    print '输入文件目录：'
+    data_dir = raw_input()
+    print '输入laser保存pose 的文件'
+    laser_file = raw_input()
+    laser_file = data_dir + laser_file
+    print '输入保存wifi信号的文件'
+    wifi_file = raw_input()
+    wifi_file = data_dir + wifi_file
+    print '生成mac_list文件中'
+    mac_list = get_mac_list(wifi_file)
+    print '转变wifi 为 向量形式'
+    print 'year'
+    year = raw_input()
+    print 'month'
+    month = raw_input()
+    print 'day'
+    day = raw_input()
+    print 'hours'
+    hours = raw_input()
+    file_trance(wifi_file , year, month, day, hours)
+    print '成功，保存转化后的wifi数据到文件 out_wifi.txt'
+    print '开始下一步，同步时间轴'
+    syn_data(laser_file, wifi_file = 'out_wifi.txt', pose_out = 'end_pose.txt', wifi_out = 'end_wifi.txt')
+    print '完成数据转化，保存结果到 end_pose.txt 和 end_wifi.txt'
--- svm.py
+++ svm.py
@@ -3,29 +3,38 @@ __author__ = 'steve'
 from sklearn import svm
 import data_transfor
 import numpy
-#wifi, pose = read_data.read_data()
-wifi = numpy.loadtxt('wifi_end.txt')
-pose = numpy.loadtxt('pose.txt')
-print 'pose',pose
-#以1.5m为间隔将数据离散化，
+
+def svm_test(wifi_file,pose_file):
+    #wifi, pose = read_data.read_data()
+    wifi = numpy.loadtxt(wifi_file)
+    pose = numpy.loadtxt(pose_file)
+
+    print 'pose',pose
+    #以1.5m为间隔将数据离散化，
+    pose_label, pose_lable_dict = data_transfor.pose_to_label(pose, 1.5)
+    clf = svm.SVC()
+    print 'wifi', wifi
+    print clf.fit(wifi, pose_label)#训练
+
+    print 'poselabel', pose_label#输出，可以看出分了多少各类别
+    err = 0
+    err_times = 0
+    ans = numpy.zeros(len(pose_label))
+    for i in range(0,len(pose_label)-1):
+        ans[i] = clf.predict(wifi[i, :])
+        err += (ans[i] - pose_label[i]) * (ans[i] - pose_label[i])
+        if abs(ans[i] - pose_label[i]) > 2:
+            err_times  = err_times + 1
+            print 'ans:',ans[i],'pose_label: ',pose_label[i]#显示错误分类的结果，和真实类别
+        #print err,'  ',err_times,' i:',i, 'i-err_times:' , i-err_times
+    print 'err:',err,'err_times:',err_times
+    print err_times*100.0/len(pose_label) ,' %'#分类错误率
 
 
-pose_label, pose_lable_dict = data_transfor.pose_to_label(pose, 1.5)
-
-clf = svm.SVC()
-print clf.fit(wifi, pose_label)#训练
-print 'wifi', wifi
-print 'wifi', wifi
-print 'poselabel', pose_label#输出，可以看出分了多少各类别
-err = 0
-err_times = 0
-ans = numpy.zeros(len(pose_label))
-for i in range(0,len(pose_label)-1):
-    ans[i] = clf.predict(wifi[i, :])
-    err += (ans[i] - pose_label[i]) * (ans[i] - pose_label[i])
-    if abs(ans[i] - pose_label[i]) > 2:
-        err_times  = err_times + 1
-        print 'ans:',ans[i],'pose_label: ',pose_label[i]#显示错误分类的结果，和真实类别
-    #print err,'  ',err_times,' i:',i, 'i-err_times:' , i-err_times
-print 'err:',err,'err_times:',err_times
-print err_times*100.0/len(pose_label) ,' %'#分类错误率
+######################################################################
+#
+#
+#
+#######################################################################
+if __name__ == '__main__':
+    svm_test('end_wifi.txt','end_pose.txt')
